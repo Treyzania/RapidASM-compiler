575e24e11bf8a92ecaab166df40ef9c86f3b37c3
treyzania <treyzania@gmail.com>
2016-02-27 15:29:53 -0500

    Set up ModuleBuilder and main class code so that the compiler can test if a module file is valid.

--- src/main/java/net/rapidasm/ModuleBuilder.java
+++ src/main/java/net/rapidasm/ModuleBuilder.java
@@ -1,6 +1,16 @@
 package net.rapidasm;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import net.rapidasm.antlr.RapidASMLexer;
+import net.rapidasm.antlr.RapidASMParser;
+import net.rapidasm.antlr.RapidWalkerController;
 
 public class ModuleBuilder {
 
@@ -14,9 +24,32 @@ public class ModuleBuilder {
 	
 	public Module createModule(File file) {
 		
-		// TODO ANTLR stuff to do this.
+		Module mod = new Module();
 		
-		return null;
+		try {
+			
+			ANTLRInputStream stream = new ANTLRInputStream(new FileReader(file));
+			
+			// Basic pipeline setup.
+			RapidASMLexer lexer = new RapidASMLexer(stream);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			RapidASMParser parser = new RapidASMParser(tokens);
+			
+			// Walker setup & walk.
+			ParseTreeWalker walker = new ParseTreeWalker();
+			RapidWalkerController ctrl = new RapidWalkerController();
+			walker.walk(ctrl, parser.module());
+			
+			// Populate the module.
+			mod.sections.addAll(ctrl.sectionsEncountered);
+			mod.filename = file;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		return mod;
 		
 	}
 	
--- src/main/java/net/rapidasm/RapidASM.java
+++ src/main/java/net/rapidasm/RapidASM.java
@@ -1,10 +1,21 @@
 package net.rapidasm;
 
+import net.rapidasm.structure.subroutines.RapidSection;
+
 public class RapidASM {
 	
 	public static void main(String[] args) {
 		
+		String fileName = args[0];
 		
+		ModuleBuilder mb = new ModuleBuilder();
+		Module mod = mb.createModule(fileName);
+		
+		System.out.println("done: " + mod);
+		System.out.println("sections: (" + mod.sections.size() + ")");
+		for (RapidSection s : mod.sections) {
+			System.out.println("\t" + s.name);
+		}
 		
 	}
 	
