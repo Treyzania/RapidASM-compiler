7c4514b499dd1f6436b126c6f263e16a70d0f9ac
treyzania <treyzania@gmail.com>
2016-03-09 12:29:23 -0500

    Added basic implemenation for outputting assembly code.

--- src/main/java/net/rapidasm/Module.java
+++ src/main/java/net/rapidasm/Module.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.rapidasm.structure.Assemblable;
+import net.rapidasm.structure.Headerable;
 import net.rapidasm.structure.subroutines.RapidSection;
 
-public class Module {
+public class Module implements Assemblable, Headerable {
 
 	public File filename;
 	
@@ -15,11 +17,21 @@ public class Module {
 	public Module() {
 		this.sections = new ArrayList<>();
 	}
-	
-	public HeaderFile getHeader() {
+
+	@Override
+	public void addLines(BinarySource src) {
 		
-		// TODO Header file generation.
-		return null;
+		// Add beginning stuff.
+		src.addComment("Generated from " + this.toString());
+		// TODO Add some generic stuff derived from the arch.
+		
+		// Put in the sections.
+		for (RapidSection sec : this.sections) {
+			
+			sec.addLines(src);
+			src.addSpace();
+			
+		}
 		
 	}
 	
--- src/main/java/net/rapidasm/structure/subroutines/RapidSection.java
+++ src/main/java/net/rapidasm/structure/subroutines/RapidSection.java
@@ -3,10 +3,14 @@ package net.rapidasm.structure.subroutines;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.rapidasm.BinarySource;
 import net.rapidasm.Module;
+import net.rapidasm.structure.Assemblable;
 import net.rapidasm.structure.Child;
+import net.rapidasm.structure.symbols.LabelSymbol;
+import net.rapidasm.structure.symbols.Labelable;
 
-public class RapidSection implements Child<Module> {
+public class RapidSection implements Child<Module>, Assemblable {
 
 	private Module module;
 	public String name;
@@ -29,6 +33,50 @@ public class RapidSection implements Child<Module> {
 	@Override
 	public Module getStructuralParent() {
 		return this.module;
+	}
+	
+	@Override
+	public void addLines(BinarySource src) {
+		
+		String actualName = this.name.toLowerCase();
+		
+		if (this.name.equals("TEXT")) {
+			actualName = "text";
+		} else if (this.name.equals("DATA")) {
+			actualName = "data";
+		} else if (this.name.equals("BSS")) {
+			actualName = "bss";
+		}
+		
+		src.addLabel("rsm_section_" + this.name);
+		src.addCode("section." + actualName);
+		
+		for (SectionPopulant child : this.children) {
+			
+			// Put in the lines of code if the child supports them.
+			if (child instanceof Assemblable) {
+				
+				Assemblable asm = (Assemblable) child;
+				
+				// Check for labels, generate them if so.
+				if (asm instanceof Labelable) {
+					
+					List<LabelSymbol> labels = ((Labelable) asm).getLabels();
+					for (LabelSymbol label : labels) {
+						src.addLabel(label.getName());
+					}
+					
+				}
+				
+				asm.addLines(src);
+				src.addSpace();
+				
+			}
+			
+		}
+		
+		src.addLabel("rsm_section_" + this.name + "_end");
+		
 	}
 	
 }
