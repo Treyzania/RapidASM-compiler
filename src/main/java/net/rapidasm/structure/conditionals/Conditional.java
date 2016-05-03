3487640df943580cda8b7050dfd2aef0fe1912cc
treyzania <treyzania@gmail.com>
2016-05-02 20:07:15 -0400

    Fixed Conditional mappings.

--- src/main/java/net/rapidasm/structure/conditionals/Conditional.java
+++ src/main/java/net/rapidasm/structure/conditionals/Conditional.java
@@ -5,11 +5,11 @@ import net.rapidasm.arch.Instruction;
 public enum Conditional {
 
 	EQUAL(Instruction.JUMP_EQUAL, Instruction.JUMP_INEQUAL, "=="),
-	INEQUAL(Instruction.JUMP_INEQUAL, Instruction.JUMP_EQUAL, "<>"),
+	INEQUAL(Instruction.JUMP_INEQUAL, Instruction.JUMP_EQUAL, "!=", "<>"),
 	GRATER_THAN(Instruction.JUMP_GREATER, Instruction.JUMP_LESS_OR_EQUAL, ">"),
 	GREATER_THAN_EQUAL(Instruction.JUMP_GREATER_OR_EQUAL, Instruction.JUMP_LESS, ">=", "=>"),
 	LESS_THAN(Instruction.JUMP_LESS, Instruction.JUMP_GREATER_OR_EQUAL, "<"),
-	LESS_THAN_EQUAL(Instruction.JUMP_LESS_OR_EQUAL, Instruction.JUMP_GREATER, "<=", "=>");
+	LESS_THAN_EQUAL(Instruction.JUMP_LESS_OR_EQUAL, Instruction.JUMP_GREATER, "<=", "=<");
 	
 	public String[] operators;
 	public Instruction jmpInstruction;
